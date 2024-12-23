d4e686dee225618dc01128f5d5351ab8b697e994
ricardo.roque <ricardo.roque@neomind.com.br>
2024-12-23 19:16:02 -0300

    First commit

---
+++ src/main/java/org/example/criptoproject/CriptoProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.criptoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriptoProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CriptoProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/criptoproject/CriptoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.criptoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriptoProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
